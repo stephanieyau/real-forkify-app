ad8dd7aef7f72f7ef6f9cdf8c14c173c1ad0eb72
stephanieyau <stephanieyau8384@gmail.com>
2022-07-08 23:48:56 +1000

    Upload Recipe async function

--- src/js/controller.js
+++ src/js/controller.js
@@ -104,8 +104,14 @@ const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
-const controlAddRecipe = function (newRecipe) {
-  console.log(newRecipe);
+const controlAddRecipe = async function (newRecipe) {
+  try {
+    //Upload the new recipe data
+    await model.uploadRecipe(newRecipe);
+  } catch (err) {
+    console.error('spobbita', err);
+    addRecipeView.renderError(err.message);
+  }
 };
 
 const init = function () {
